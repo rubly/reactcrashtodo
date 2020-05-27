ebdfe99250a0a6cc9e59a079ba3f50ee7a0fa6e2
Bryan Morgan <Bryan.Morgan@optimumcredit.co.uk>
2020-05-27 13:36:52 +0100

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is TodoList app v.0.0.1</p>
+        </React.Fragment>
+    )
+}
